75f5d51118c5ba35a083ef86eae1e6a6a7e6d303
Ivo <ivol@Ivos-MacBook-Air.local>
2018-02-28 16:56:38 -0600

    adding more functionality and updating the table

--- bamazon.sql
+++ bamazon.sql
@@ -1,14 +1,19 @@
+DROP DATABASE IF EXISTS bamazonDB;
 create database bamazonDB;
 use bamazonDB;
 CREATE TABLE products (
     id INT NOT NULL AUTO_INCREMENT,
     product_name varchar (100) NOT NULL,
     department_name VARCHAR(50) NULL,
-    price decimal(10,4) NULL,
+    price decimal(10,2) NULL,
     stock_quantity int NULL,
     PRIMARY KEY (id)
 );
 SELECT * FROM bamazonDB.products;
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-Values ("Go-pro 5",  "Electronics", 350, 10), ("Rose Water",  "Beauty", 11.99, 25)
+Values ("GoPro Hero5",  "Electronics", 299.50, 15), ("Rose Water",  "Beauty", 11.95, 30),
+	   ("Probiotic",  "Health", 17, 50), ("Bead Bracelets",  "Jewelry", 15.99, 15),
+       ("Day Cream",  "Beauty", 12.50, 25), ("Javascript for Kids",  "Books", 17, 31), 
+       ("Forrest Gump",  "DVD", 11, 24), ("Goggles",  "Sports", 17, 5), 
+       ("Samsonite Carry-on",  "Luggage", 95, 15), ("Yoga Mat",  "Sports", 30, 50)
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,111 +1,136 @@
+// dependency for inquirer npm package
 var inquirer = require("inquirer");
-
+//// dependency for mysql npm package
 var mysql = require("mysql");
+// dependency for console.table npm package
 require("console.table");
-
 
 // create the connection information for the sql database
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-
-  // Your username
-  user: "root",
-
-  // Your password
-  password: "",
-  database: "bamazonDB"
+    host: "localhost",
+    port: 3306,
+    
+    // Your username
+    user: "root",
+    
+    // Your password or leave it blank if no password is required
+    password: "",
+    database: "bamazonDB"
 });
 
 // connect to the mysql server and sql database
 connection.connect(function(err) {
-  if (err) throw err;
-  // run the start function after the connection is made to prompt the user
-  console.log("connected as id " + connection.threadId);
-  start();
-});
-
-  function start() {
-      // query the database for all items available for sale
-  connection.query("SELECT * FROM products", function(err, results) {
     if (err) throw err;
-    console.table(results);
-    // once you have the items, prompt the user for which they'd like to buy
-    inquirer
-    .prompt([
-      {
-        name: "id",
-        type: "rawlist",
-        choices: function() {
-          var productsArray = [];
-          for (var i = 0; i < results.length; i++) {
-            productsArray.push(results[i].product_name);
-          }
-          return productsArray;
-        },
-        message: "What item would you like to buy?"
-      },
-      {
-        name: "quantity",
-        type: "input",
-        message: "How many would you like to buy?",
-        validate: function(value) {
-            if (isNaN(value) === false) {
-              return true;
-            }
-            return false;
-          }
-      }
-    ])
-    .then(function(answer) {
-        // get the information of the chosen item
-        var chosenItem;
-        for (var i = 0; i < results.length; i++) {
-          if (results[i].product_name === answer.id) {
-            chosenItem = results[i];
-          }
-        }
-
-        if (chosenItem.stock_quantity > parseInt(answer.quantity)) {
-            // there is enough quantity in stock
-            connection.query(
-              "UPDATE products SET ? WHERE ?",
-              [
-                {
-                  stock_quantity: (chosenItem.stock_quantity - answer.quantity) 
+    console.log("connected as id " + connection.threadId);
+    // run the start function after the connection is made to prompt the user
+    start();
+});
+//Start function that includes all of the user's activity and response
+function start() {
+    // query the database for all items available for sale
+    connection.query("SELECT * FROM products", function(err, results) {
+        if (err) throw err;
+        //displays the table with the available inventory
+        console.table(results);
+        // once the items are displayed, prompt the user for which they'd like to buy 
+        inquirer
+        .prompt([
+            {
+                name: "id",
+                type: "rawlist",
+                choices: function() {
+                    var productsArray = [];
+                    for (var i = 0; i < results.length; i++) {
+                        productsArray.push(results[i].product_name);
+                    }
+                    return productsArray;
                 },
-                {
-                  id: chosenItem.id
+                message: "What item would you like to buy?"
+            },
+            //prompt for quantity that user wants to purchase
+            {
+                name: "quantity",
+                type: "input",
+                message: "How many would you like to buy?",
+                validate: function(value) {
+                    if (isNaN(value) === false) {
+                        return true;
+                    }
+                    return false;
                 }
-              ],
-              function(error) {
-                if (error) throw err;
-                console.log("\n" + "\x1b[32mOrder placed successfully!\x1b[0m");
-                console.log("Your total is: " + ("\x1b[32m" + "$" + answer.quantity * chosenItem.price + "\x1b[0m" + "\n")); 
+            }
+        ])
+        .then(function(answer) {
+            // get the information of the chosen item
+            var chosenItem;
+            for (var i = 0; i < results.length; i++) {
+                if (results[i].product_name === answer.id) {
+                    chosenItem = results[i];
+                }
+            }
+            //if there is enough inventory from the chosen item proceed with the sale
+            if (chosenItem.stock_quantity > parseInt(answer.quantity)) {
+                connection.query(
+                    "UPDATE products SET ? WHERE ?",
+                    [
+                        { 
+                            //updating the quantity left in stock after the sale
+                            stock_quantity: (chosenItem.stock_quantity - answer.quantity) 
+                        },
+                        {
+                            id: chosenItem.id
+                        }
+                    ],
+                    //if there is no error triggered inform the user of the successful transaction and the total price
+                    function(error) {
+                        if (error) throw err;
+                        console.log("\n" + "\x1b[32mOrder placed successfully!\x1b[0m");
+                        console.log("Your total is: " + ("\x1b[32m" + "$" + answer.quantity * chosenItem.price + "\x1b[0m" + "\n")); 
+                        //prompting if the user wants to shop more 
+                        inquirer.prompt([{
+                            name: 'order',
+                            type: 'confirm',
+                            message: 'Would you like to place another order?'
+                        }]).then(function (response) {
+                            if (response.order) {
+                                console.log("\n" + "\x1b[34mPlease see the product choices in the table!\x1b[0m");
+                                //if confirmed that user want to make another purchase restart the app
+                                start();
+                            } else {
+                                console.log("\n" + "\x1b[34mBye! Come again!\x1b[0m");
+                            }
+                        });
+                    }
+                );
+            }
+            //if there is not enough quantity in stock
+            else {
+                console.log("\n" + "\x1b[31mInsufficient quantity! See the table above and try again...\x1b[0m" + "\n");
+                //and prompt the user if he wants to order a different quantity 
                 inquirer.prompt([{
                     name: 'order',
                     type: 'confirm',
-                    message: 'Would you like to place another order?'
+                    message: 'Would you like to try again with different quantity or different item?'
                 }]).then(function (response) {
                     if (response.order) {
-                        console.log("\n" + "\x1b[34mPlease see the product choices in the table!\x1b[0m");
+                        console.log("\n" + "\x1b[34mPlease see the available quantities in the table!\x1b[0m");
+                        //if confirmed that user want to adjust the quantity or pick another item restart the app
                         start();
                     } else {
                         console.log("\n" + "\x1b[34mBye! Come again!\x1b[0m");
                     }
-                });
-
-                // start();
-              }
-            );
-          }
-          else {
-              //not enough quantity in stock
-            console.log("\x1b[31mInsufficient quantity! See the table above and try again...\x1b[0m");
-            start(); 
-          }
-    
+                }); 
+            }
+            
         });
     });
-  }
-  
+}
+
+//   if (chosenItem.stock_quantity === 0){
+//     connection.query(
+//         "DELETE FROM products WHERE ?",
+//         {
+//           stock_quantity: 0
+//         },
+//     );
+// }
